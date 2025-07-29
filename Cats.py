662dd33e0e00883d13d3bb5b26ea0c746bc4f975
Sheerjoy <alexnus@mail.ru>
2025-07-29 21:36:34 +0300

    Созданы окно и виджеты

--- Cats.py
+++ Cats.py
@@ -1,1 +1,21 @@
 from tkinter import *
+from PIL
+import Image, ImageTk
+import requests from io i
+mport (BytesIO)
+
+window = Tk()
+window.title("Cats!")
+window.geometry("600x480")
+
+label = Label()
+label.pack() url = 'https://cataas.com/cat'
+
+url = 'https://cataas.com/cat'
+img = load_image(url)
+
+if img:
+    label.config(image=img)
+    label.image = img
+
+window.mainloop()
