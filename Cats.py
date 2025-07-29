b15acb14b87c63c89e4b9ad237ff9bd670977f81
Sheerjoy <alexnus@mail.ru>
2025-07-29 21:40:21 +0300

    Создана функция для получения изображения с котиком

--- Cats.py
+++ Cats.py
@@ -4,6 +4,17 @@ import Image, ImageTk
 import requests from io i
 mport (BytesIO)
 
+def load_image(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        image_data = BytesIO(response.content)
+        img = Image.open(image_data)
+        return ImageTk.PhotoImage(img)
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
+        return None
+
 window = Tk()
 window.title("Cats!")
 window.geometry("600x480")
